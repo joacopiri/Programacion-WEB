586ddd0ee295e8153fbbd570004db3f1b2272b35
Joaquin Pirillo <joaquinpirillo5102007@gmail.com[3~>
2025-09-10 17:04:04 -0300

    Avance con lo que pude

--- ACTIVIDADES JS/Ejercicio 2 DOM/script.js
+++ ACTIVIDADES JS/Ejercicio 2 DOM/script.js
@@ -2,7 +2,6 @@
 
 Mostrar cuántos productos hay en total y cuántos ya se compraron.*/
 
-
 let boton = document.getElementById("boton")
 
 boton.addEventListener("click", function(){
@@ -11,23 +10,40 @@ boton.addEventListener("click", function(){
     let button2 = document.createElement("button")
         button2.textContent = "Eliminar"  
     let li = document.createElement("li")
+    let liP = document.createElement("liP")
+    let liC = document.createElement("liC")
 
     let container = document.getElementById("container")
     let text = document.getElementById("text").value
     let obj = {elemento: text}
 
-    li.textContent = obj.elemento;
+    li.textContent = obj.elemento
     li.append(button1)
     li.append(button2)
     container.append(li)
 
+    if(boton = ("click")){
+        liP +1
+    }
     button1.addEventListener("click", function(){
         li.classList.toggle("tachado")
+        if(li.classList = "tachado"){
+            liC -1
+        }
+        else if(li.classList = ("")){
+            liC +1
+        }
     })
     button2.addEventListener("click", function(){
         li.remove()
+        if(button2 = ("click")){
+            liP -1
+            liC -1
+        }
     })
 })
+
+/*--------------------------------------------------------------*/
 
 document.addEventListener("keydown", function(event){
     if (event.key === "Enter") {
