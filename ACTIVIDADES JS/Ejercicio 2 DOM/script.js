efd74188230f0dd3bb89ee1bce58aedb115200fa
Joaquin Pirillo <joaquinpirillo5102007@gmail.com>
2025-09-24 13:30:44 -0300

    Termine el ejercicio 2 de DOM

--- ACTIVIDADES JS/Ejercicio 2 DOM/script.js
+++ ACTIVIDADES JS/Ejercicio 2 DOM/script.js
@@ -1,49 +1,64 @@
-/*Guardar la lista en localStorage para que no se borre al recargar.
+/*Guardar la lista en localStorage para que no se borre al recargar.*/
 
-Mostrar cuántos productos hay en total y cuántos ya se compraron.*/
+let liTValue = 0 
+let resultadoT = document.createElement("h3")
+let containerT = document.getElementById("containerT")
+containerT.append(resultadoT)
+resultadoT.append(liTValue)
+
+let liCValue = 0 
+let resultadoC = document.createElement("h3")
+let containerC = document.getElementById("containerC")
+containerC.append(resultadoC)
+resultadoC.append(liCValue)
 
 let boton = document.getElementById("boton")
 
 boton.addEventListener("click", function(){
-    let button1 = document.createElement("button")
-        button1.textContent = "Comprado"  
-    let button2 = document.createElement("button")
-        button2.textContent = "Eliminar"  
+    let botonC = document.createElement("button")
+        botonC.textContent = "Comprado"  
+    let botonE = document.createElement("button")
+        botonE.textContent = "Eliminar"  
     let li = document.createElement("li")
-    let liP = document.createElement("liP")
-    let liC = document.createElement("liC")
-
     let container = document.getElementById("container")
     let text = document.getElementById("text").value
     let obj = {elemento: text}
 
     li.textContent = obj.elemento
-    li.append(button1)
-    li.append(button2)
+    li.append(botonC)
+    li.append(botonE)
     container.append(li)
 
-    if(boton = ("click")){
-        liP +1
-    }
-    button1.addEventListener("click", function(){
+    liTValue += 1
+    resultadoT.textContent = liTValue
+    localStorage.setItem("Productos totales", liTValue);
+
+    botonC.addEventListener("click", function(){
         li.classList.toggle("tachado")
-        if(li.classList = "tachado"){
-            liC -1
+        if(li.classList == "tachado"){
+            liCValue += 1 
+            resultadoC.textContent = liCValue
+            localStorage.setItem("Productos comprados", liCValue);
         }
-        else if(li.classList = ("")){
-            liC +1
+        else if(li.classList == ("")){
+            liCValue -= 1 
+            resultadoC.textContent = liCValue
+            localStorage.setItem("Productos comprados", liCValue);
         }
     })
-    button2.addEventListener("click", function(){
+    botonE.addEventListener("click", function(){
         li.remove()
-        if(button2 = ("click")){
-            liP -1
-            liC -1
+        liTValue -= 1
+        resultadoT.textContent = liTValue
+        localStorage.setItem("Productos totales", liTValue);
+        if(liCValue === 0){
+            return 
         }
+        liCValue -= 1
+        resultadoC.textContent = liCValue
+        localStorage.setItem("Productos comprados", liCValue);
     })
 })
-
-/*--------------------------------------------------------------*/
 
 document.addEventListener("keydown", function(event){
     if (event.key === "Enter") {
