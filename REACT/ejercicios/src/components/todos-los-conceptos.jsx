2f568e1bec5260ce7732a565735c107bb9c946d7
Joaquin Pirillo <joaquinpirillo5102007@gmail.com>
2025-10-29 14:53:24 -0300

    termine los primeros ejercicios

--- REACT/ejercicios/src/components/todos-los-conceptos.jsx
+++ REACT/ejercicios/src/components/todos-los-conceptos.jsx
@@ -1,33 +1,47 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function Conceptos() {
-  const [texto, setTexto] = useState(''); 
-  const [textoB, setTextoB] = useState('presioname');
-  const [mostrar, setMostrar] = useState(false)
-  
+  const [texto, setTexto] = useState("");
+  const [textoB, setTextoB] = useState("Presioname");
+  const [mostrar, setMostrar] = useState(false);
+
   const manejarCambio = (event) => {
-    setTexto(event.target.value); 
+    setTexto(event.target.value);
   };
   const mostrarNombre = () => {
-    setMostrar(true)
+    setMostrar(true);
+  };
+
+  let contador = 4;
+  const manejarClick = () => {
+    setTextoB("Gracias por presionar");
+    console.log("Saludo actualizado");
+    const interval = setInterval(() => {
+      contador--;
+      console.log("contador", contador);
+      setTextoB(contador);
+      if (contador === 1) {
+        return;
+      }
+    }, 1000);
+    setTimeout(() => {
+      setTextoB("Presioname");
+      clearInterval(interval);
+    }, 4000);
   };
 
   return (
     <div>
-    <h3>ingresa tu nombre</h3>
-    <div>
-      <input
-        type="text"
-        value={texto} 
-        onChange={manejarCambio}
-      />
-    </div>
-    <div>
-      {mostrar === false ? <>Debe ingresar un nombre: {texto}</> : texto}
-    </div>
-    <div>
-      <button onClick={()=> setTextoB("Gracias por presionar"), console.log("Saludo actualizado")}>{textoB}</button>
-    </div>
+      <h3>ingresa tu nombre</h3>
+      <div>
+        <input type="text" value={texto} onChange={manejarCambio} />
+      </div>
+      <div>
+        {mostrar === false ? <>Debe ingresar un nombre: {texto}</> : texto}
+      </div>
+      <div>
+        <button onClick={manejarClick}>{textoB}</button>
+      </div>
     </div>
   );
 }
