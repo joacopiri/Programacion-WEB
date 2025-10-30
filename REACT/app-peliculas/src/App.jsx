4fbde0761246ceea52ba9ac4edbed3a5a9a36533
joacopiri <joaquinpirillo5102007@gmail.com>
2025-10-30 00:33:03 -0300

    avance con el proyecto

--- REACT/app-peliculas/src/App.jsx
+++ REACT/app-peliculas/src/App.jsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [peliculas, setPeliculas] = useState({});
-
   const [text, setText] = useState("");
-
   const [input, setInput] = useState("");
 
   useEffect(() => {
-    if (input.trim() === "") return; // evita llamar la API si está vacío
+    if (input.trim() === "") return;
     fetch(`https://www.omdbapi.com/?t=${input}&apikey=f0ef6fdc`)
       .then((data) => data.json())
-      .then((response) => setPeliculas(response));
+      .then((response) => {
+        setPeliculas(response);
+        console.log(response);
+      });
   }, [input]);
 
   const handleClick = () => {
@@ -29,11 +29,24 @@ function App() {
       <div>
         <h2>Buscador de Peliculas</h2>
       </div>
+
       <div className="div">
-        <input value={text} onChange={handleInput}></input>
+        <input 
+          value={text} 
+          onChange={handleInput}
+        />
       </div>
+
       <div className="div">
         <button onClick={handleClick}>buscar</button>
+      </div>
+
+      <div className="div">
+        {peliculas.Response === "True" ? 
+          <div>
+            <h3>{peliculas.Title} "{peliculas.Year}"</h3>
+            <img src={peliculas.Poster}/>
+          </div> : ""}
       </div>
     </>
   );
