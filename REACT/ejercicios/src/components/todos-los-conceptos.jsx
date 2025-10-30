b28cef47def52ae9941a053366a0e983ba2dc717
joacopiri <joaquinpirillo5102007@gmail.com>
2025-10-29 22:58:14 -0300

    termine de ajustar un error en el ultimo ejercicio

--- REACT/ejercicios/src/components/todos-los-conceptos.jsx
+++ REACT/ejercicios/src/components/todos-los-conceptos.jsx
@@ -4,28 +4,38 @@ function Conceptos() {
   const [texto, setTexto] = useState("");
   const [textoB, setTextoB] = useState("Presioname");
   const [mostrar, setMostrar] = useState(false);
+  const [bloqueado, setBloqueado] = useState(false); 
 
   const manejarCambio = (event) => {
     setTexto(event.target.value);
   };
+
   const mostrarNombre = () => {
     setMostrar(true);
   };
 
-  let contador = 4;
+  
   const manejarClick = () => {
+    if(bloqueado) return;
+    setBloqueado(true);
+
     setTextoB("Gracias por presionar");
     console.log("Saludo actualizado");
+
+    let contador = 4;
+  
     const interval = setInterval(() => {
       contador--;
       console.log("contador", contador);
       setTextoB(contador);
       if (contador === 1) {
-        return;
+        clearInterval(interval);
       }
     }, 1000);
+
     setTimeout(() => {
       setTextoB("Presioname");
+      setBloqueado(false);
       clearInterval(interval);
     }, 4000);
   };
